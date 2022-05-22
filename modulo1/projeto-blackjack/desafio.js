ef4a685230668ec2a0cf6afe9062698c7d8fe89b
Ilena Acioli dos Santos <ilenaaciolids@gmail.com>
2022-05-22 17:40:38 -0300

    Projeto-blackjack: Desafio completo

--- modulo1/projeto-blackjack/desafio.js
+++ modulo1/projeto-blackjack/desafio.js
@@ -16,35 +16,67 @@ let carta = comprarCarta()
 let rodadas = 0
 let cartasUsuario = []
 let cartasComputador = []
-let pontuacaooUsuario = 0
+let pontuacaoUsuario = 0
 let pontuacaoComputador = 0
+let fimDoJogo = false
 
 
-console.log("Boas vindas ao jogo Blackjack!")
 
-if (confirm("Quer iniciar uma nova rodada?")) {
-   while (rodadas < 2) {
-      carta = comprarCarta()
-      let somaDeCartasUsuario = cartasUsuario.push(carta.texto)
-      pontuacaooUsuario = Number(pontuacaooUsuario + (carta.valor))
-      carta = comprarCarta()
-      let somaDeCartasComputador = cartasComputador.push(carta.texto)
-      pontuacaoComputador = Number(pontuacaoComputador + (carta.valor))
-      if (rodadas == 0 && pontuacaoComputador == 11 && pontuacaooUsuario == 11) {
+if (confirm("Boas vindas ao jogo Blackjack! \n Quer iniciar uma nova rodada?")) {
+   while (fimDoJogo == false && pontuacaoUsuario < 21) {
+      if (rodadas == 0 && pontuacaoComputador == 11 && pontuacaoUsuario == 11) {
          cartasUsuario = []
          cartasComputador = []
-         pontuacaooUsuario = 0
+         pontuacaoUsuario = 0
          pontuacaoComputador = 0
       }
+      if (rodadas <= 1) {
+         carta = comprarCarta()
+         let somaDeCartasComputador = cartasComputador.push(carta.texto)
+         pontuacaoComputador = Number(pontuacaoComputador + (carta.valor))
+         carta = comprarCarta()
+         let somaDeCartasUsuario = cartasUsuario.push(carta.texto)
+         pontuacaoUsuario = Number(pontuacaoUsuario + (carta.valor))
+
+      }
+      while (fimDoJogo == false && rodadas > 1 && pontuacaoUsuario < 21) {
+         if (confirm(`Suas cartas são ${cartasUsuario}. A carta revelada do computador é ${cartasComputador[0]}` +
+            "\n" +
+            "Deseja comprar mais uma carta?")) {
+            carta = comprarCarta()
+            let somaDeCartasUsuario = cartasUsuario.push(carta.texto)
+            pontuacaoUsuario = Number(pontuacaoUsuario + (carta.valor))
+         } else {
+            while (pontuacaoComputador < pontuacaoUsuario) {
+               carta = comprarCarta()
+               let somaDeCartasComputador = cartasComputador.push(carta.texto)
+               pontuacaoComputador = Number(pontuacaoComputador + (carta.valor))
+            }
+            fimDoJogo = true
+         }
+      }
+
       rodadas = rodadas + 1
    }
-   console.log(`Usuário - cartas: ${cartasUsuario} - pontuação ${pontuacaooUsuario}`)
-   console.log(`Computador - cartas: ${cartasComputador} - pontuação ${pontuacaoComputador}`)
-   if (pontuacaooUsuario > pontuacaoComputador) {
-      console.log("O usuário ganhou!")
-   } else if (pontuacaoComputador > pontuacaooUsuario) {
-      console.log("O computador ganhou!")
-   } else { console.log("Empate!") }
+
+
+   if (pontuacaoUsuario >= 21) {
+      alert(
+         `Suas cartas são ${cartasUsuario} . Sua pontuação é ${pontuacaoUsuario}.\n ` +
+         `As cartas do computador são ${cartasComputador} . A pontuação do computador é ${pontuacaoComputador}. \n` +
+         `O computador ganhou!`)
+
+   } else if (pontuacaoComputador > pontuacaoUsuario && pontuacaoUsuario < 21) {
+      alert(
+         `Suas cartas são ${cartasUsuario} . Sua pontuação é ${pontuacaoUsuario}.\n ` +
+         `As cartas do computador são ${cartasComputador} . A pontuação do computador é ${pontuacaoComputador}. \n` +
+         `O usuário ganhou!`)
+   } else {
+      alert(
+         `Suas cartas são ${cartasUsuario} . Sua pontuação é ${pontuacaoUsuario}.\n ` +
+         `As cartas do computador são ${cartasComputador} . A pontuação do computador é ${pontuacaoComputador}. \n` +
+         `Empatou!`)
+   }
 
 } else {
    console.log("o jogo acabou")
