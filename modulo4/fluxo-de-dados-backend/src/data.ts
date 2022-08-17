f6b67e9912368c657d6b1482e8002656a69963f1
Ilena Acioli dos Santos <ilenaaciolids@gmail.com>
2022-08-17 18:26:03 -0300

    exercicios fluxo-de-dados-backend

---
+++ modulo4/fluxo-de-dados-backend/src/data.ts
@@ -0,0 +1,11 @@
+type Product = {
+    id:string,
+    name: string,
+    price: number
+}
+
+export let products:Product[] = [
+   { id: "1", name: "celular", price: 1500},
+   { id: "2", name: "tablet", price: 2000},
+   { id: "3", name: "computador", price: 3000},
+]
